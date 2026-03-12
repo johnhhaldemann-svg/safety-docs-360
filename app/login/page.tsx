990c8baecc719628a31e2f3d92231a8c9ca7bd69
John Haldemann <john.h.haldemann@gmail.com>
2026-03-12 14:08:54 -0500

    debug supabase env

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,6 +9,12 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
 
+console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+console.log(
+  "SUPABASE KEY START:",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 20)
+);
+
 export default function LoginPage() {
   const router = useRouter();
 
